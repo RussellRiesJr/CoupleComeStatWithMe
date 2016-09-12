b5e68656d24f5b768f231eb190d8687e841a0701
Russell Ries Jr <riesru@gmail.com>
2016-09-12 13:42:25 -0500

    add models to models.py

--- ccswm/statApi/models.py
+++ ccswm/statApi/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
 # Create your models here.
+
+
+seasons = [(number, number) for number in range(1, 4)]
+episodes = [(number, number) for number in range(1, 70)]
+nights = [(number, number) for number in range(1, 3)]
+scores = [(number, number) for number in range(0, 20)]
+totals = [(number, number) for number in range(0, 40)]
+
+
+class Episode(models.Model):
+    season = models.IntegerField(choices=seasons, default=1)
+    episodeNumber = models.IntegerField(choices=episodes, default=1)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.location
+
+
+class Starter(models.Model):
+    protein = models.CharField(max_length=100)
+    side = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.protein
+
+
+class Entree(models.Model):
+    protein = models.CharField(max_length=100)
+    side = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.protein
+
+
+class Dessert(models.Model):
+    primary = models.CharField(max_length=100)
+    secondary = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.primary
+
+
+class Entertainment(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
+
+
+class CoupleMeal(models.Model):
+    starter = models.ForeignKey(Starter, related_name='meal')
+    entree = models.ForeignKey(Entree, related_name='meal')
+    dessert = models.ForeignKey(Dessert, related_name='meal')
+    entertainment = models.ForeignKey(Entertainment, related_name='meal')
+
+
+class Couple(models.Models):
+    nightNumber = models.IntegerField(choices=nights, default=1)
+    ageRange = models.CharField(max_length=6)
+    sexPref = models.CharField(max_length=15)
+    mrtlStat = models.CharField(max_length=15)
+    mrtlLength = models.CharField(max_length=15)
+    haveTheme = models.BooleanField
+    theme = models.CharField(max_length=30)
+    foodEth = models.CharField(max_length=30)
+    episode = models.ForeignKey(Episode, related_name='couple')
+    coupleMeal = models.ForeignKey(CoupleMeal, related_name='couple')
+
+
+class Results(models.Model):
+    couple = models.ForeignKey(Couple, related_name='results')
+    oppAVote = models.IntegerField(choices=scores, default=0)
+    oppBVote = models.IntegerField(choices=scores, default=0)
+    totalScore = models.IntegerField(choices=totals, default=0)
+    outcome = models.CharField(max_length=15)
